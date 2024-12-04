661da5a7200555226ed2c9cf6f35f75797c90ce5
Oli Loades <oli.loades@gmail.com>
2024-12-04 13:24:08 +0000

    Day 1 - part 2

--- 1/main.py
+++ 1/main.py
@@ -16,6 +16,16 @@ def find_diff(first_list, second_list):
         diff_sum += abs(first_list[x] - second_list[x])
     return diff_sum
 
+def find_sim(first_list, second_list):
+    sim_sum = 0
+    for x in first_list:
+        sim_sum += second_list.count(x)*x
+    return sim_sum
+
 input = get_input()
+
 diff = find_diff(input[0], input[1])
 print(diff)
+
+sim = find_sim(input[0], input[1])
+print(sim)
