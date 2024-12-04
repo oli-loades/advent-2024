ea3991ec0b0a47139e4bf335ac48c59058d5c15d
Oli Loades <oli.loades@gmail.com>
2024-12-04 16:21:19 +0000

    Added day 3 - part 2

--- 3/main.py
+++ 3/main.py
@@ -9,6 +9,22 @@ def part_one():
     for line in f:
        for match in re.findall(pattern, line):
             total += int(match[0]) * int(match[1])
-    print(total)
+    print("part one: ", total)
+
+def part_two():
+    f = open(INPUT_FILENAME, "r")
+    pattern = r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))"
+    total = 0
+    disabled = False
+    for line in f:
+       for match in re.findall(pattern, line):
+            if not disabled and match[0] != "" and match[1] != "":
+                total += int(match[0]) * int(match[1])
+            if match[2] != "":
+                disabled = False
+            if match[3] != "":
+                disabled = True      
+    print("part two: ", total)
 
 part_one()
+part_two()
